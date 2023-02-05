33ed8858e91e8db6464a7a20a83306495e424ea3
AdrielMS <adrielsjahranie@gmail.com>
2023-02-06 01:00:57 +0700

    modified:   src/App.js 	modified:   package-lock.json 	modified:   package.json 	modified:   src/App.js 	modified:   src/pages/register/index.js 	modified:   src/pages/registerHire/index.js

--- src/App.js
+++ src/App.js
@@ -9,14 +9,13 @@ import RegisterHire from "./pages/registerHire";
 import LoginOption from "./pages/loginOption";
 import Login from "../src/pages/login/index";
 import Home from "../src/pages/home/index";
+import EditProfile from "./pages/editProfile";
 
 import configureStore from "./redux/store";
 
 import { Provider } from "react-redux";
 import React from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-
-
 
 function App() {
   const { store } = configureStore();
@@ -26,13 +25,14 @@ function App() {
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<LandingPage />} />
-            <Route path="/login" element={<Login/>} />
+            <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route path="/home" element={<Home />} />
             <Route path="/userProfile/:id" element={<UserProfile />} />
             <Route path="/halamanHire" element={<HalamanHire />} />
             <Route path="/registerHire" element={<RegisterHire />} />
             <Route path="/loginOption" element={<LoginOption />} />
+            <Route path="/editProfile/:id" element={<EditProfile />} />
           </Routes>
         </BrowserRouter>
       </Provider>
